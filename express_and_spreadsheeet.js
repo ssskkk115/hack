fa3e3f7432371a8ac6b9de5464098eefacd4615a
tseigo <tnk.seigo+bitbucket@gmail.com>
2019-10-26 22:59:24 +0900

    1000円ランダムのベース実装（飲み物1、食べ物1の取得）

--- express_and_spreadsheeet.js
+++ express_and_spreadsheeet.js
@@ -16,6 +16,9 @@ app.get('/apple', function(req, res) {
 });
 
 app.get('/random', async function(req, res) {
+
+    let basePrice = 1000;  // 1000円スタート
+
     // axios でスプレッドシートのURLからCSV形式で取得 foodData
     let foodResponse = await axios.get('https://docs.google.com/spreadsheets/d/e/2PACX-1vQg_P87vLW61nngA4KXupPO4cTReuUcfDm4v0KGbocn3uJ0016YMl8NfzELjjwllB4_sg2L2g5pC3ch/pub?gid=0&single=true&output=csv');
 
@@ -27,42 +30,66 @@ app.get('/random', async function(req, res) {
     randomList = [];
 
     // 読み込んだデータをJSONに変換
-    const converterFood = csvtojson({
+    let converterFood = csvtojson({
         noheader: false,  // 1行目がヘッダーの場合はfalse
         output: "csv"
     });
-    
-    const foodJSONData = await converterFood.fromString(foodResponse.data);
+    let foodJSONData = await converterFood.fromString(foodResponse.data);
 
-    const converterDrink = csvtojson({
+    let converterDrink = csvtojson({
         noheader: false,  // 1行目がヘッダーの場合はfalse
         output: "csv"
     });
-    const drinkJSONData = await converterDrink.fromString(drinkResponse.data);
+    let drinkJSONData = await converterDrink.fromString(drinkResponse.data);
+
+    // sortPrice 価格でソート（配列の中身で細かくソート）
+    foodJSONData = sortPrice(foodJSONData);
+    drinkJSONData = sortPrice(drinkJSONData);
 
     // ランダム取得
-    let randFoodID = Math.floor(Math.random() * foodJSONData.length);
-    let randFoodData = foodJSONData[randFoodID];
-    let randDrinkID = Math.floor(Math.random() * drinkJSONData.length);
-    let randDrinkData = drinkJSONData[randDrinkID];
+    let currentItem;
+    let firstFoodItem = getBottomPriceRandom( foodJSONData , basePrice );
+    // はじめのフードが選択
+    // console.log(firstFoodItem.length);
+    if(firstFoodItem.length == 1){
+        let _oneitem = firstFoodItem[0];
+        currentItem = {
+            "id": _oneitem[0],
+            "name": _oneitem[1],
+            "price": Number(_oneitem[2]),
+            "image": _oneitem[3],
+            "oshi_message": _oneitem[4],
+            "oshi_image": _oneitem[5],
+        }
 
-    randomList.push({
-        "id": randFoodData[0],
-        "name": randFoodData[1],
-        "price": randFoodData[2],
-        "image": randFoodData[3],
-        "oshi_message": randFoodData[4],
-        "oshi_image": randFoodData[5],
-    });
+        // 選択されたメニューぶんベース金額を減らす
+        basePrice = basePrice - currentItem.price;
+        console.log("basePrice : " + basePrice);
 
-    randomList.push({
-        "id": randDrinkData[0],
-        "name": randDrinkData[1],
-        "price": randDrinkData[2],
-        "image": randDrinkData[3],
-        "oshi_message": randDrinkData[4],
-        "oshi_image": randDrinkData[5],
-    });
+        // 登録
+        randomList.push(currentItem);
+    }
+    let firstDrinktem = getBottomPriceRandom( drinkJSONData , basePrice );
+    console.log(firstDrinktem.length);
+    // はじめのドリンクが選択
+    if(firstDrinktem.length == 1){
+        let _oneitem = firstDrinktem[0];
+        currentItem = {
+            "id": _oneitem[0],
+            "name": _oneitem[1],
+            "price": Number(_oneitem[2]),
+            "image": _oneitem[3],
+            "oshi_message": _oneitem[4],
+            "oshi_image": _oneitem[5],
+        }
+
+        // 選択されたメニューぶんベース金額を減らす
+        basePrice = basePrice - currentItem.price;
+        console.log("basePrice : " + basePrice);
+
+        // 登録
+        randomList.push(currentItem);
+    }
 
     console.log(randomList)
 
@@ -71,6 +98,67 @@ app.get('/random', async function(req, res) {
 
 });
 
+function getBottomPriceRandom(items,base_price){
 
+    console.log(base_price + "円より下回るものを残す");
 
-console.log("server start! (yoro-hackathon))");
+    let new_items = [];
+    let decided_items = [];
+
+    let item_price;
+    let item_name;
+    let decided_item_price;
+    let decided_item_name;
+
+    // base_priceより下回るものを残す
+    items.forEach(function(item){
+        item_price = Number(item[2]);
+        item_name = item[1];
+        if(base_price > item_price){
+            console.log("* OK " + item_name);
+            new_items.push(item);
+        } else {
+            console.log("NG " + item_name);
+        }
+    });
+
+    console.log("下回った候補");
+    console.log(new_items);
+
+    // もし 1以上 候補があれば、ランダム取得
+    if( new_items.length > 0 ){
+        let rand_id = Math.floor(Math.random() * new_items.length);
+        decided_item = new_items[rand_id];
+        decided_item_price = Number(decided_item[2]);
+        decided_item_name = decided_item[1];
+
+        decided_items.push(decided_item);
+
+        console.log("decided_item_price " + decided_item_price);
+        console.log("decided_item_name " + decided_item_name);
+    }
+
+    console.log("ランダム取得されたもの");
+    console.log(decided_items);
+
+    return decided_items;
+}
+
+function sortPrice(items){
+
+    items.sort(function(a, b){
+        var a_price = Number(a[2]);
+        var b_price = Number(b[2]);
+        if (a_price < b_price) {
+            return -1;
+        }
+        if (a_price > b_price) {
+            return 1;
+        }
+        return 0;
+    });
+
+    return items;
+}
+
+console.log("server start! (yoro-hackathon)");
